543cb89f28965d9680564b4098d199a87fe1b30f
Devano Alfarizy <devanoalfarizy04@gmail.com>
2024-10-21 08:48:43 +0700

    Pertemuan ketiga Pemrograman Web 2

---
+++ temu03praktek1.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Contoh Kondisi</title>
+    </head>
+    <body>
+        <?php
+        $nilai = 59;
+        if ($nilai >= 60){
+            echo "Nilai ujian anda $nilai, anda dinyatakan LULUS";
+        } else {
+            echo "Nilai ujian anda $nilai, anda dinyatakan TIDAK LULUS";
+        }
+        ?>
+    </body>
+</html>
